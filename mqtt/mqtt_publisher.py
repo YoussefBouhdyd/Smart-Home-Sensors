be91bc122c17ac2ed034157439be99b99f9e23cb
Youssef Bouhdyd <boudhdyd2004@gmail.com>
2026-02-12 00:33:50 +0100

    Refactor MQTT publisher to integrate motion sensor data and remove obsolete motion sensor publisher

--- mqtt/mqtt_publisher.py
+++ mqtt/mqtt_publisher.py
@@ -10,22 +10,30 @@ from datetime import datetime, timezone
 client = mqtt.Client()
 client.connect("localhost", 1883)
 
-while True:  
-    
-    data = {
+def read_sensor_data(room_name):
+    return {
+        "roomid": room_name+"-sensor",
         "temperature": temperature.read_value(),
         "humidity": humidity.read_value(),
         "gaz": gaz.read_value(),
+    }
+def create_room_data(sensor_id, room_name):
+    return {
+        **read_sensor_data(room_name),
         "motion": {
-            "sensor_id": "003",
+            "sensor_id": sensor_id,
             "sensor_type": "motion",
             "value": random.choice([True, False]),
             "timestamp": datetime.now(timezone.utc).isoformat(),
         }
     }
-    client.publish("home/living_room", json.dumps(data))
-
-    
-    # client.publish("home/living_room/temperature", json.dumps(temperature.read_value()))
-    # client.publish("home/living_room/humidity", json.dumps(humidity.read_value()))
-    # client.publish("home/living_room/gaz", json.dumps(gaz.read_value()))
+rooms = [
+    {"id": "001", "name": "living_room"},
+    {"id": "002", "name": "kitchen"},
+    {"id": "003", "name": "bathroom"}
+]
+while True:  
+    for room in rooms:
+        room_data = create_room_data(room["id"], room["name"])
+        client.publish(f"home/{room['name']}", json.dumps(room_data))
+        time.sleep(1)
